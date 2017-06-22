458c5fc7adc2afdf6e8f457a13645f58508ca114
dtablalis <dtablalis@gmail.com>
2017-06-22 10:27:27 +0300

    my First commit
    Change-Id: I03a6b90872edf8ab6b2669ef1880174d15cf4160
    Signed-off-by: dtablalis <dtablalis@gmail.com>

---
+++ helloWorld222/src/helloWorld222/CardReader.java
@@ -0,0 +1,18 @@
+package helloWorld222;
+
+import java.io.*;
+
+
+public class CardReader {
+	int myInt;
+	String myString;
+	CardReader card = new CardReader();
+
+	
+	
+	public void go() {
+		System.out.print("ccc");
+	}	
+}
+
+
